2642e9917eb13cfa0369c680f257fca49baac676
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-23 02:15:31 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,13 +7,13 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
-    if (isAdmin) return m.reply('*KALAU ADMIN BEBAS YA ADICK ADICK*')
-    if (!isBotAdmin) return m.reply('*LAWACK ANJIR BOT BUKAN ADMIN DISUDUH JAGA GRUP_-*')
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}*HADEH BOCIL SHARELINK!\n\nBTW ENAK DEK DI KICK`)
+    if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
     let isLinkconnGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nLAWACK ANJIR BOCIL SHARELINK!!.\nBTW ENAK DI KICK DEK?')
+    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nOrder denied, bot will not kick you.\nBecause the group link itself')
     await conn.sendMessage(m.chat, { delete: m.key })
     await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
   }
