f8cd07d8a7e1d548719bd3e0e4c3e59dc250a1ac
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-23 00:16:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 //Ini untuk setting Bot//
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx'] 
-global.mods = ['6282xxxxxx'] 
-global.prems = ['6282xxxxxx'] 
+global.owner = ['6283890585717'] 
+global.mods = ['6283890585717'] 
+global.prems = ['6283890585717'] 
 
 //『 Info Owner 』 //
-global.nameowner = '⫹⫺ Owner'
-global.numberowner = '6282xxxxxx'
-global.instagram = 'https://instagram.com/prm2.0'
-global.github = 'https://github.com/BOTCAHX'
-global.dana = '6282xxxxxx'
-global.pulsa = '6282xxxxxx'
-global.gopay = ''
+global.nameowner = '⫹⫺ Zeroo Store'
+global.numberowner = '6283890585717'
+global.instagram = 'males buat gw'
+global.github = 'https://github.com/Zeroo'
+global.dana = '085784914140'
+global.pulsa = '083830815715'
+global.gopay = 'https://tinyurl.com/2el4odke'
 
 //『 Info Bot 』//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.namebot = 'Zeroo-Md²'
+global.gc = 'https://chat.whatsapp.com/E8IwFUibPjkDgilRKw6JBg'
+global.web = 'https://tinyurl.com/2el4odke' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = 'CUMAN OPEN 1 BULAN HARGA? 3K'
 
 //『 Tampilan Dan Lainnya 』//
 global.fotonya1 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-studios?text=List%20&text2=Menu' 
 global.fotonya2 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-studios?text=List%20&text2=Menu' 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© Zeroo-Md'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ Zeroo-MD'
+global.wm3 = '© Zeroo-Md'
 global.wm4 = 'Ⓛ = Limit Ⓟ = Premium'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -40,7 +40,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\n Zeroo-Md'
 
 //『 Apikey 』 //
 global.APIs = { // API Prefix
